83705ac5c85f0a823d5ceb68c4406ad55574efd4
vijayuuu1211 <vijayuuu1211@gmail.com>
2022-09-17 15:44:03 +0530

    login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -36,10 +36,10 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
+// $app->singleton(
+//     Illuminate\Contracts\Debug\ExceptionHandler::class,
+//     App\Exceptions\Handler::class
+// );
 
 /*
 |--------------------------------------------------------------------------
